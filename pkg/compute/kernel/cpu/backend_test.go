f7bfa9f94d0fade3f479b82c71be9549c5b2081f
Daniel Owen van Dommelen <theapemachine@gmail.com>
2026-03-28 19:18:13 +0100

    test: add unit tests for tombstone propagation and unlink behavior in UniversalBitwise function

--- pkg/compute/kernel/cpu/backend_test.go
+++ pkg/compute/kernel/cpu/backend_test.go
@@ -39,6 +39,50 @@ func TestUniversalBitwise(t *testing.T) {
 	})
 }
 
+func installFirmware(frame *[128]uint64, fw core.FirmwareType) {
+	start := uint64(core.Cfg.ProgramIndex)
+	for i, w := 0, start; i < len(core.Cfg.Firmware[fw]) && int(w) < len(frame); i, w = i+2, w+1 {
+		frame[w] = firmwareWordAt(core.Cfg.Firmware[fw], i)
+	}
+}
+
+func TestTombstonePropagationAndUnlink(t *testing.T) {
+	be := NewBackend()
+	var a, b [128]uint64
+
+	installFirmware(&a, core.FirmwareTypeTombstone)
+	a[core.Cfg.R6] = 1234
+	a[core.Cfg.PreviousID] = 1234
+	a[core.Cfg.NextID] = 9999
+	b[core.Cfg.PreviousID] = 7
+	b[core.Cfg.NextID] = 1234
+
+	if err := be.UniversalBitwise(unsafe.Pointer(&a), unsafe.Pointer(&b)); err != nil {
+		t.Fatal(err)
+	}
+
+	if got := a[core.Cfg.PreviousID]; got != 0 {
+		t.Fatalf("prev was not cleared: got %d want 0", got)
+	}
+	if got := a[core.Cfg.NextID]; got != 9999 {
+		t.Fatalf("next changed unexpectedly: got %d want 9999", got)
+	}
+	if got := b[core.Cfg.R6]; got != 1234 {
+		t.Fatalf("target id not propagated: got %d want 1234", got)
+	}
+	if got, want := b[core.Cfg.ProgramIndex:], a[core.Cfg.ProgramIndex:]; fmt.Sprint(got) != fmt.Sprint(want) {
+		t.Fatalf("tombstone program not propagated")
+	}
+
+	installFirmware(&b, core.FirmwareTypeTombstone)
+	if err := be.UniversalBitwise(unsafe.Pointer(&b), unsafe.Pointer(&a)); err != nil {
+		t.Fatal(err)
+	}
+	if got := b[core.Cfg.NextID]; got != 0 {
+		t.Fatalf("infected next was not cleared: got %d want 0", got)
+	}
+}
+
 func BenchmarkUniversalBitwise(b *testing.B) {
 	be := NewBackend()
 	var a, c [128]uint64
